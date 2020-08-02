1a22a4a60a0a5431d516929bff2f968036c6d626
Sanoj Hettige <sanojhettige@ip-192-168-8-100.ec2.internal>
2020-08-02 19:20:37 +0530

    add database

---
+++ db/ase_paddy_storage.sql
@@ -0,0 +1,417 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Aug 02, 2020 at 07:20 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ase_paddy_storage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collection_centers`
+--
+
+CREATE TABLE `collection_centers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `city` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` text COLLATE utf8_unicode_ci NOT NULL,
+  `capacity` decimal(11,2) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `collection_centers`
+--
+
+INSERT INTO `collection_centers` (`id`, `name`, `city`, `address`, `phone_number`, `capacity`, `created_at`, `created_by`, `modified_at`, `modified_by`, `status`) VALUES
+(1, 'Center 01', 'Colombo', 'test', '0123456789', '10000.00', '2020-08-02 18:41:20', 1, '2020-08-02 13:11:20', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `collection_center_cash_book`
+--
+
+CREATE TABLE `collection_center_cash_book` (
+  `id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `amount` decimal(11,2) NOT NULL,
+  `received_date` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paddy_buying_allocations`
+--
+
+CREATE TABLE `paddy_buying_allocations` (
+  `id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `max_amount` decimal(11,3) NOT NULL,
+  `min_amount` decimal(11,3) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paddy_expections`
+--
+
+CREATE TABLE `paddy_expections` (
+  `id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `from_date` datetime NOT NULL,
+  `to_date` date NOT NULL,
+  `amount` decimal(11,3) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paddy_prices`
+--
+
+CREATE TABLE `paddy_prices` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `price` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `purchase_id` int(11) NOT NULL,
+  `farmer_user_id` int(11) NOT NULL,
+  `paid_amount` decimal(11,2) NOT NULL,
+  `paid_date` datetime NOT NULL,
+  `pay_notes` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `id` int(11) NOT NULL,
+  `farmer_user_id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `collection_date` datetime NOT NULL,
+  `collected_amount` decimal(11,3) NOT NULL,
+  `collected_rate` decimal(11,2) NOT NULL,
+  `purchase_notes` text COLLATE utf8_unicode_ci,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `issue_date` datetime NOT NULL,
+  `issue_amount` decimal(11,3) NOT NULL,
+  `sales_notes` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfers`
+--
+
+CREATE TABLE `transfers` (
+  `id` int(11) NOT NULL,
+  `from_collection_center_id` int(11) NOT NULL,
+  `to_collection_center_id` int(11) NOT NULL,
+  `vehilcle_id` int(11) NOT NULL,
+  `amount` decimal(11,3) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `collection_center_id`, `name`, `email`, `password`, `created_at`, `created_by`, `modified_at`, `modified_by`, `status`) VALUES
+(1, 1, 0, 'Sanoj Hettige', 'sanojhettige@gmail.com', '123', '2020-08-02 15:10:38', 1, '2020-08-02 09:40:38', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`id`, `name`) VALUES
+(5, 'Clerk'),
+(3, 'Collection Officer'),
+(6, 'Farmer'),
+(2, 'Finance Officer'),
+(1, 'Manager'),
+(4, 'Storekeeper');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL,
+  `registration_number` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `vehicle_type` int(11) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `collection_centers`
+--
+ALTER TABLE `collection_centers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `collection_center_cash_book`
+--
+ALTER TABLE `collection_center_cash_book`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paddy_buying_allocations`
+--
+ALTER TABLE `paddy_buying_allocations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paddy_expections`
+--
+ALTER TABLE `paddy_expections`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paddy_prices`
+--
+ALTER TABLE `paddy_prices`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `date` (`date`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transfers`
+--
+ALTER TABLE `transfers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `registration_number` (`registration_number`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `collection_centers`
+--
+ALTER TABLE `collection_centers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `collection_center_cash_book`
+--
+ALTER TABLE `collection_center_cash_book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paddy_buying_allocations`
+--
+ALTER TABLE `paddy_buying_allocations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paddy_expections`
+--
+ALTER TABLE `paddy_expections`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paddy_prices`
+--
+ALTER TABLE `paddy_prices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transfers`
+--
+ALTER TABLE `transfers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
