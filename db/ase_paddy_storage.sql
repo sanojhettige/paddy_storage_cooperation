8fd636e4ffbd2c8f71b0430ae0bbed4cc719a327
Sanoj Hettige <sanojhettige@Sanojs-MacBook-Pro.local>
2020-08-05 10:52:30 +0530

    db updates

--- db/ase_paddy_storage.sql
+++ db/ase_paddy_storage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Aug 02, 2020 at 07:20 PM
+-- Generation Time: Aug 05, 2020 at 10:51 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -35,7 +35,7 @@ CREATE TABLE `collection_centers` (
   `capacity` decimal(11,2) NOT NULL,
   `created_at` datetime NOT NULL,
   `created_by` int(11) NOT NULL,
-  `modified_at` timestamp NOT NULL,
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_by` int(11) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -45,7 +45,7 @@ CREATE TABLE `collection_centers` (
 --
 
 INSERT INTO `collection_centers` (`id`, `name`, `city`, `address`, `phone_number`, `capacity`, `created_at`, `created_by`, `modified_at`, `modified_by`, `status`) VALUES
-(1, 'Center 01', 'Colombo', 'test', '0123456789', '10000.00', '2020-08-02 18:41:20', 1, '2020-08-02 13:11:20', 1, 1);
+(2, 'Collection Center 01', 'shdb', 'sdfsd', '01234567', '444.00', '2020-08-05 09:05:48', 1, '2020-08-05 03:35:48', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -64,6 +64,34 @@ CREATE TABLE `collection_center_cash_book` (
   `modified_by` int(11) NOT NULL,
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farmers`
+--
+
+CREATE TABLE `farmers` (
+  `id` int(11) NOT NULL,
+  `collection_center_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `nic_no` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `address` text COLLATE utf8_unicode_ci NOT NULL,
+  `phone_number` varchar(22) COLLATE utf8_unicode_ci NOT NULL,
+  `land_size` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_by` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `farmers`
+--
+
+INSERT INTO `farmers` (`id`, `collection_center_id`, `name`, `nic_no`, `address`, `phone_number`, `land_size`, `created_at`, `created_by`, `modified_at`, `modified_by`, `status`) VALUES
+(2, 2, 'Farmer 01', '1234567890v', 'Galle', '0712345678', '10Acre', '2020-08-05 10:50:31', 1, '2020-08-05 05:20:31', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -273,6 +301,12 @@ ALTER TABLE `collection_center_cash_book`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `farmers`
+--
+ALTER TABLE `farmers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `paddy_buying_allocations`
 --
 ALTER TABLE `paddy_buying_allocations`
@@ -344,13 +378,19 @@ ALTER TABLE `vehicles`
 -- AUTO_INCREMENT for table `collection_centers`
 --
 ALTER TABLE `collection_centers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `collection_center_cash_book`
 --
 ALTER TABLE `collection_center_cash_book`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `farmers`
+--
+ALTER TABLE `farmers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `paddy_buying_allocations`
