f9436d6b8d7548d1e2a7c2f229617c0187c0c604
Sanoj Hettige <sanojhettige@ip-192-168-8-104.ec2.internal>
2020-08-17 20:08:54 +0530

    all with tcpdf

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 if(!isset($_SESSION)) {
     session_start();
 }
-define("APP_PATH", "App");
-define("BASE_URL", "http://esoft-paddy.local:8888");
 
 require_once "App/Config/config.php";
 require_once "Core/Controller.php";
 require_once "Core/Model.php";
 require_once "Core/View.php";
+require_once "Core/Library.php";
 require_once "Core/App.php";
 
 require_once "Core/Helper.php";
